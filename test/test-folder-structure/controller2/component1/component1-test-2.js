c641e30c8a5eb990182fd8b16277ea2543d32725
Nick Yue <yuez0002@gmail.com>
2018-12-04 10:44:33 +0800

    Added component test for test-folder-structure; Added test_main.js as test entry; Modify package.json to initiate test_main.js

---
+++ test/test-folder-structure/controller2/component1/component1-test-2.js
@@ -0,0 +1,7 @@
+const mochaddt = require('../../../../index');
+
+describe("component1-test-2", function() {
+    it("should run", function() {
+
+    });
+});
---
+++ test/test-folder-structure/controller2/component1/component1-test.js
@@ -0,0 +1,7 @@
+const mochaddt = require('../../../../index');
+
+describe("component1-test-1", function() {
+    it("should run", function() {
+
+    });
+});
